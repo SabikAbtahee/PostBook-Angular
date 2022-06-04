3ba8d7d60ef63d0c8e8e70394ccfeecc581716c7
SabikAbtahee <sabikchamp@gmail.com>
2022-06-04 06:32:51 +0600

    chore: authentication modules and components created

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,12 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { SetNewPasswordComponent } from './components/set-new-password/set-new-password.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'sign-in'
+	},
+	{
+		path: 'sign-in',
+		component: SignInComponent
+	},
+	{
+		path: 'sign-up',
+		component: SignUpComponent
+	},
+	{
+		path: 'forget-password',
+		component: ForgotPasswordComponent
+	},
+	{
+		path: 'reset-password',
+		component: SetNewPasswordComponent
+	}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+	declarations: [
+		SignUpComponent,
+		SignInComponent,
+		ForgotPasswordComponent,
+		SetNewPasswordComponent
+	],
+	imports: [CommonModule, RouterModule.forChild(routes)]
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
