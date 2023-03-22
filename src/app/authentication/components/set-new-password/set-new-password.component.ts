f64a4bbb1e461c9c7c562a8236e350946f90c0a3
SabikAbtahee <sabikchamp@gmail.com>
2023-03-22 14:28:47 +0600

    feat: set new password page made

--- src/app/authentication/components/set-new-password/set-new-password.component.ts
+++ src/app/authentication/components/set-new-password/set-new-password.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UtilsService } from '@shared';
+import { Subject } from 'rxjs';
+import { AuthenticationService } from '../../services/authentication.service';
+import { environment } from '@environment';
 
 @Component({
 	selector: 'app-set-new-password',
@@ -6,7 +11,34 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./set-new-password.component.scss']
 })
 export class SetNewPasswordComponent implements OnInit {
-	constructor() {}
+	setPasswordForm: FormGroup;
+	isLoading: boolean;
+	_unsubscribeAll: Subject<any>;
 
-	ngOnInit(): void {}
+	constructor(
+		private fb: FormBuilder,
+		private authService: AuthenticationService,
+		private utilService: UtilsService
+	) {
+		this._unsubscribeAll = new Subject();
+	}
+
+	ngOnInit(): void {
+		this.initForm();
+	}
+
+	initForm() {
+		this.setPasswordForm = this.fb.group({
+			Password: [null, [Validators.required, Validators.pattern(environment.PasswordRegex)]],
+			ConfirmPassword: [null, [Validators.required, Validators.pattern(environment.PasswordRegex)]]
+		});
+	}
+
+	submit() {}
+
+	ngOnDestroy() {
+		this._unsubscribeAll.next(true);
+		this._unsubscribeAll.complete();
+		this._unsubscribeAll.unsubscribe();
+	}
 }
